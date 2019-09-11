01e05c2ecc6e6e9f14d552a737d09d247a417263
Ubuntu <ubuntu@ip-172-31-39-240.us-east-2.compute.internal>
2019-09-11 01:40:20 +0000

    hyperopt for g3.16xlarge

--- hyperopt/hyper.py
+++ hyperopt/hyper.py
@@ -8,15 +8,14 @@ import ray
 from ray.tune import run, sample_from
 from ray.tune.schedulers import AsyncHyperBandScheduler
 from ray.tune.suggest.hyperopt import HyperOptSearch
+from hyperopt import hp
 
 if __name__ == "__main__":
-    ray.init(num_gpus=7, redis_password="hyperopt")
-    import argparse
-    from hyperopt import hp
+    ray.init(redis_password="hyperopt")
     space = {
         "lr": hp.choice("lr", [1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6]),
         "gamma": hp.choice("gamma", [0.997, 0.995, 0.99, 0.98, 0.97, 0.95, 0.9, 0.85, 0.8]),
-        "entropy_coeff": hp.choice("entropy_coeff", [0, 0.001, 0.01])
+        "entropy_coeff": hp.choice("entropy_coeff", [0.0, 0.001, 0.01])
     }
     current_best_params = [ 
         {
@@ -37,12 +36,12 @@ if __name__ == "__main__":
             "sample_batch_size": 100,
             "sgd_minibatch_size": 500,
             "num_sgd_iter": 10,
-            "num_workers": 5,
+            "num_workers": 15,
             "num_envs_per_worker": 5,
             "batch_mode": "truncate_episodes",
             "observation_filter": "NoFilter",
             "vf_share_layers": True,
-            "num_gpus": 0.875,
+            "num_gpus": 1,
             "env": "BreakoutNoFrameskip-v4",
             # TODO: Are these necessary?
             #"lr": 1e-3,
@@ -50,12 +49,12 @@ if __name__ == "__main__":
             #"entropy_coeff": 0.001,
         },
         "stop": {
-            "timesteps_total": 20000000,
+            "timesteps_total": 50000000,
         },
     }
     algo = HyperOptSearch(
         space,
-        max_concurrent=8,
+        max_concurrent=4,
         metric="episode_reward_mean",
         mode="max",
         #points_to_evaluate=current_best_params
@@ -63,7 +62,7 @@ if __name__ == "__main__":
     scheduler = AsyncHyperBandScheduler(metric="episode_reward_mean", mode="max")
     run(
         "PPO",
-        name="hyperopt_breakout_trial_2",
+        name="hyperopt_breakout_50m",
         search_alg=algo,
         #scheduler=scheduler,
         **config
